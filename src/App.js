ea3d0d5384239726820d8dc55f7db9f8ed71d8c7
Msk6 <Maha.s.kh@hotmail.com>
2020-10-07 16:35:08 +0300

    done choose difficulty by the player

--- src/App.js
+++ src/App.js
@@ -1,34 +1,20 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
-
-//imags
-import cardBack from './images/CardBack.jpg'
-import allCards  from './data';
-
-//components
-import Card from './components/Card'
-
-//utils
-import {shuffle} from './utils'
-
+import Difficulty from "./components/Difficulty";
+import Game from "./components/Game";
 
 function App() {
-
-  const [cards, setCards] = useState(shuffle([...allCards, ...allCards]));
-
-  const cardList = cards.map((card, idx) => {
-    return <Card card={card} key={`${card.id}-${idx}`}/>
-  })
+  const [difficulty, setDifficulty] = useState(null);
 
   return (
-  <div className="App border my-5">
-    <div className="container">
-      <div className="row">
-        {cardList}
-      </div>
+    <div className="App border my-5">
+      {difficulty ? (
+        <Game difficulty={difficulty} />
+      ) : (
+        <Difficulty setDifficulty={setDifficulty} />
+      )}
     </div>
-  </div>
-  )
+  );
 }
 
 export default App;
